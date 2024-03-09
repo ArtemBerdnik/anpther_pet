73e65bb18e8a97ede7c3859e311c78d379722535
Artem_Berdnik <Artem_Berdnik@epam.com>
2024-03-09 20:03:51 +0200

    updates for chromeDriverManager

--- test/conftest.py
+++ test/conftest.py
@@ -2,13 +2,12 @@ import allure
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.driver_cache import DriverCacheManager
 
 
 @pytest.fixture()
 @allure.title("Prepare for the test: Browser set up")
 def driver():
-    driver = webdriver.Chrome(ChromeDriverManager(cache_manager=DriverCacheManager(valid_range=-1)).install())
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
     yield driver
     driver.quit()
